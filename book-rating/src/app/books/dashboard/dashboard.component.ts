1c842f85fd3a1236310c70992367270575354e74
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-04-23 15:01:26 +0200

    duck typing

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { Book } from '../shared/book';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  books = ['Angular', 'jQuery', 'Angular'];
+  // 🦆
+  books: Book[] = [{
+    isbn: '000',
+    title: 'Angular',
+    description: 'Tolles Buch',
+    rating: 1
+  }];
 
 }
 
