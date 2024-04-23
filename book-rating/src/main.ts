cd76f371814957a6e71af987c0a011b685860c94
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-04-23 12:03:07 +0200

    typscript playground

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Test } from './test';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
+
+
+
+
+const test = new Test(42);
---
+++ book-rating/src/test.ts
@@ -0,0 +1,10 @@
+
+const x = 2;
+
+export class Test {
+
+
+  constructor(public zahl: number) {
+    console.log('😎 Test', this.zahl, x);
+  }
+}
