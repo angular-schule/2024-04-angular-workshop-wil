5180a2f365b9441b710184d01e11361fa221d204
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-05-02 09:44:17 +0200

    markAllAsTouched

--- book-rating/src/app/books/book-create/book-create.component.ts
+++ book-rating/src/app/books/book-create/book-create.component.ts
@@ -44,6 +44,11 @@ export class BookCreateComponent {
 
   submitForm() {
 
+    if (this.bookForm.invalid) {
+      this.bookForm.markAllAsTouched();
+      return;
+    }
+
     const newBook: Book = {
       ...this.bookForm.getRawValue(),
       rating: 1
