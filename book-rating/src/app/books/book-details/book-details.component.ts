d6ebe97c4a9a4d2056a1a1ab9b77b98c90bdf1a1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-05-03 14:37:57 +0200

    rxjs: finales beispiel

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,9 +1,11 @@
+import { JsonPipe } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { concatMap, map, mergeMap, switchMap } from 'rxjs';
+import { catchError, map, of, retry, switchMap } from 'rxjs';
+
 import { BookStoreService } from '../shared/book-store.service';
-import { JsonPipe } from '@angular/common';
 
 
 @Component({
@@ -20,8 +22,16 @@ export class BookDetailsComponent {
   book$ = toSignal(
     inject(ActivatedRoute).paramMap.pipe(
       map(paramMap => paramMap.get('isbn') || ''),
-      switchMap(isbn => this.bookStore.getSingleBook(isbn))
+      switchMap(isbn => this.bookStore.getSingleBook(isbn).pipe(
+        retry({
+          count: 3,
+          delay: 200
+        }),
+        catchError((err: HttpErrorResponse) => of({
+          title: 'ERROR',
+          description: err.message
+        }))
+      ))
     )
-  )
-
+  );
 }
