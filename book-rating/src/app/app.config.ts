cbc272231906307183b83be14f2ae927e2b4a26f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-05-03 14:38:05 +0200

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools ng add @ngrx/schematics

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, /* withDebugTracing() */),
-    provideHttpClient()
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+]
 };
 
 
