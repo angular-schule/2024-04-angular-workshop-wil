a9f884e370f006948b03a5c9370bf0aa3c3b7082
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-05-03 14:41:06 +0200

    provideState(bookFeature),    provideEffects(BookEffects)

--- book-rating/src/app/app.config.ts
+++ book-rating/src/app/app.config.ts
@@ -3,9 +3,11 @@ import { provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { bookFeature } from './books/store/book.reducer';
+import { BookEffects } from './books/store/book.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +15,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideStore(),
     provideEffects(),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+
+    provideState(bookFeature),
+    provideEffects(BookEffects)
+
 ]
 };
 
